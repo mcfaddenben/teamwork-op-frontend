93907287b1ff27bd9ef676e2bf2134d6a3bcb3b9
Ben McFadden <mcfaddenben64@gmail.com>
2021-07-06 01:55:23 -0400

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +11,16 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/Login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/Signup",
+    name: "signup",
+    component: Signup,
   },
   {
     path: "/about",
