eca822dc463f5914aa5b3a66c1cb011885f9923b
Ben McFadden <mcfaddenben64@gmail.com>
2021-07-07 00:45:34 -0400

    adds profile page and login token compatability

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,16 @@
       |
       <router-link to="/index">Index</router-link>
       |
-      <router-link to="/login">Login</router-link>
-      |
-      <router-link to="/signup">Signup</router-link>
-      |
-      <router-link to="/logout">Logout</router-link>
+      <span v-if="isLoggedIn()">
+        <router-link to="/logout">logout</router-link>
+        |
+        <router-link :to="`/users/${getUserId()}`">Profile</router-link>
+      </span>
+      <span v-else>
+        <router-link to="/signup">Signup</router-link>
+        |
+        <router-link to="/login">Login</router-link>
+      </span>
     </div>
     <router-view />
   </div>
@@ -39,3 +44,15 @@
   color: #42b983;
 }
 </style>
+<script>
+export default {
+  methods: {
+    isLoggedIn: function () {
+      return localStorage.getItem("jwt");
+    },
+    getUserId: function () {
+      return localStorage.user_id;
+    },
+  },
+};
+</script>
